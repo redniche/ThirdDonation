dd37c589ae03f906e7d12f2745fcd7b63dc5bc41
redniche <redniche@naver.com>
2022-03-01 09:38:51 +0900

    #S06P21E207-14: Create backend server Spring Boot project
    Init spring boot project in feature/backend
    depedencies
    - lombok
    - swagger
    - servlet(jstl)
    build plugins
    - org.springframework.boot
    - io.spring.dependency-management
    - java
    - war
    - idea
    - checkstyle

---
+++ third_donation_server/src/main/java/com/thirdlife/thirddonation/controller/MongoTestController.java
@@ -0,0 +1,87 @@
+package com.thirdlife.thirddonation.controller;
+
+import com.thirdlife.thirddonation.dto.UserTest;
+import com.thirdlife.thirddonation.repository.UserTestRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//테스트용 Controller입니다. 절대 이를 따라서 만들지 말길
+/**
+ * Test Controller with MongoDB.
+ */
+@RestController
+@CrossOrigin(origins = { "*" }, maxAge = 6000)
+@Api("Test 컨트롤러 API V1") // Controller에 대한 설명 Swagger Annotation
+@RequestMapping("/test")
+public class MongoTestController {
+
+    public static final Logger logger = LoggerFactory.getLogger(MongoTestController.class);
+    private static final String SUCCESS = "success";
+    private static final String FAIL = "fail";
+
+    @Autowired
+    private UserTestRepository userTestRepository;
+
+    /**
+     * Find all users information in MongoDB.
+     *
+     * @return ResponseEntity instance
+     */
+    @ApiOperation(value = "모든유저조회", notes = "모든유저를 조회함", response = Map.class)
+    @GetMapping("")
+    public ResponseEntity<Map<String, Object>> findAllUserTest() {
+        logger.debug("findAllUser............................");
+        Map<String, Object> responseMap = new HashMap<>();
+        HttpStatus status = HttpStatus.ACCEPTED;
+        try {
+            List<UserTest> userList = userTestRepository.findAll();
+            responseMap.put("userList", userList);
+        } catch (Exception e) {
+            logger.error("회원정보 등록 실패 : {}", e);
+            responseMap.put("message", e.getMessage());
+            status = HttpStatus.INTERNAL_SERVER_ERROR;
+        }
+        return new ResponseEntity<Map<String, Object>>(responseMap, status);
+    }
+
+    /**
+     * Insert user information into MongoDB.
+     *
+     * @param user UserTest instance
+     * @return ResponseEntity instance
+     */
+    @ApiOperation(value = "유저집어넣기", notes = "유저를 집어넣음", response = Map.class)
+    @PostMapping("")
+    public ResponseEntity<Map<String, Object>> insertUser(@RequestBody @ApiParam UserTest user) {
+        logger.debug("regist............................");
+        logger.debug("user:{}", user);
+        Map<String, Object> resultMap = new HashMap<>();
+        HttpStatus status = HttpStatus.ACCEPTED;
+        try {
+            userTestRepository.insert(user);
+            resultMap.put("message", SUCCESS);
+            System.out.println(user);
+        } catch (Exception e) {
+            logger.error("회원정보 등록 실패 : {}", e);
+            resultMap.put("message", e.getMessage());
+            status = HttpStatus.INTERNAL_SERVER_ERROR;
+        }
+        return new ResponseEntity<Map<String, Object>>(resultMap, status);
+    }
+
+}
---
+++ third_donation_server/src/test/java/com/thirdlife/thirddonation/ThirdDonationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thirdlife.thirddonation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThirdDonationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

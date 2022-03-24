986fd1e053a93439e529861c8f8aad2f3ee08ff9
redniche <redniche@naver.com>
2022-03-25 04:35:55 +0900

    Update contracts for token exist check
    토큰 존재 체크를 위해 컨트랙 수정

--- third_donation_blockchain/contracts/SsafyNFT.sol
+++ third_donation_blockchain/contracts/SsafyNFT.sol
@@ -10,8 +10,19 @@ import "./access/Ownable.sol";
  * 상태 변수나 함수의 시그니처는 구현에 따라 변경할 수 있습니다.
  */
 contract SsafyNFT is ERC721Enumerable, ERC721URIStorage, Ownable {
+    using Strings for uint256;
     // 예술가 여부를 매핑함. O(1)
     mapping(address => bool) private _artistAddress;
+
+    // 같은 파일 해시 여부를 매핑함. O(1)
+    mapping(string => bool) private _isUsedFile;
+
+    // 같은 tokenURI 존재 여부를 매핑함. O(1)
+    mapping(string => bool) private _isUsedTokenURI;
+
+    constructor() ERC721("SsafyNFT", "SFT") {
+        _artistAddress[owner()] = true;
+    }
 
     /**
      * @dev Throws if called by any account other than the artist.
@@ -22,7 +33,12 @@ contract SsafyNFT is ERC721Enumerable, ERC721URIStorage, Ownable {
         _;
     }
 
-    constructor() ERC721("SsafyNFT", "SFT") {}
+    /**
+     * @dev file과 tokenURI를 입력받아 동일 토큰의 존재 여부를 반환함.
+     */
+    function _existToken(string memory file, string memory _tokenURI) internal view virtual returns (bool) {
+        return _isUsedFile[file] || _isUsedTokenURI[_tokenURI];
+    }
 
     function addArtistAddress(address artistAddress) public onlyOwner {
         _artistAddress[artistAddress] = true;
@@ -52,12 +68,22 @@ contract SsafyNFT is ERC721Enumerable, ERC721URIStorage, Ownable {
         return super.tokenURI(tokenId);
     }
 
-    function create(address to, string memory _tokenURI) public onlyArtist returns (uint256) {
+    function create(
+        address to,
+        string memory file,
+        string memory _tokenURI
+    ) public onlyArtist returns (uint256) {
+        require(!_existToken(file, _tokenURI), "SsafyNFT: Already exist token");
         uint256 newTokenId = totalSupply() + 1;
+
         //NFT 민팅
         super._mint(to, newTokenId);
 
-        //토큰 아아디리에 해당하는 URI 변경
+        //token 값들을 존재하는 토큰으로 변경
+        _isUsedFile[file] = true;
+        _isUsedTokenURI[_tokenURI] = true;
+
+        //토큰 아아디에 해당하는 URI 변경
         super._setTokenURI(newTokenId, _tokenURI);
 
         //반환

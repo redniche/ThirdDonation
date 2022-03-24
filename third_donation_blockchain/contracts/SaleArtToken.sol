9963cee25a8204c817a8c76be44cae294f6d3799
김동주 <rlaehdwn23@naver.com>
2022-03-24 16:16:04 +0900

    update sale art nft contract - 민팅 매개변수 tokenURI로 변경 - 작품 구매 함수 추가

--- third_donation_blockchain/contracts/SaleArtToken.sol
+++ third_donation_blockchain/contracts/SaleArtToken.sol
@@ -26,10 +26,43 @@ contract SaleArtToken {
         // 해당 작품이 판매중이 아닐 때
         require(artTokenPrices[_artTokenId] == 0, "This art token is already on sale");
         // artTokenOwner가 판매 스마트 컨트랙트(address(this))에 권한을 넘겼는지 확인
-        // require(mintArtTokenAddress.isApprovedForAll(artTokenOwner, address(this)),"Art token owner did not approve token");
+        require(mintArtTokenAddress.isApprovedForAll(artTokenOwner, address(this)),"Art token owner did not approve token");
         
         artTokenPrices[_artTokenId] = _price;
 
         onSaleArtTokkenArray.push(_artTokenId);
     }
+
+    // 작품 구매
+    function purchaseArtToken(uint256 _artTokenId) public payable{
+        uint256 price = artTokenPrices[_artTokenId];
+        address artTokenOwner = mintArtTokenAddress.ownerOf(_artTokenId);
+
+        require(price > 0, "Art Token is not sale.");
+        require(price <= msg.value, "Caller sent lower than price.");
+        require(artTokenOwner != msg.sender, "Caller is art token owner.");
+
+        payable(artTokenOwner).transfer(msg.value);
+        mintArtTokenAddress.safeTransferFrom(artTokenOwner, msg.sender, _artTokenId);
+
+        artTokenPrices[_artTokenId] = 0;
+
+        for(uint256 i = 0; i < onSaleArtTokkenArray.length; i++){
+            if(artTokenPrices[onSaleArtTokkenArray[i]] == 0){
+                onSaleArtTokkenArray[i] = onSaleArtTokkenArray[onSaleArtTokkenArray.length-1];
+                onSaleArtTokkenArray.pop();
+            }
+
+        }
+    }
+
+    // 판매 중인 작품 배열의 길이 출력
+    function getOnSaleArtTokenArrayLength() view public returns (uint256) {
+        return onSaleArtTokkenArray.length;
+    }
+
+    function getArtTokenPrice(uint256 _artTokenId) view public returns (uint256){
+        return artTokenPrices[_artTokenId];
+    }
+
 }
--- third_donation_blockchain/contracts/SsafyNFT.sol
+++ third_donation_blockchain/contracts/SsafyNFT.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.4;
 import "./token/ERC721/extensions/ERC721Enumerable.sol";
 import "./token/ERC721/extensions/ERC721URIStorage.sol";
 import "./access/Ownable.sol";
+import "./SaleArtToken.sol";
 
 /**
  * PJT Ⅰ - 과제 2) NFT Creator 구현
@@ -11,25 +12,26 @@ import "./access/Ownable.sol";
  */
 contract SsafyNFT is ERC721Enumerable, ERC721URIStorage, Ownable {
    
-    // 작품 구조체
-    struct Art{
-        uint id;
-        string title;
-        string artist;
-        string description;
-    }
+    // // 작품 구조체
+    // struct Art{
+    //     uint id;
+    //     string title;
+    //     string artist;
+    //     string description;
+    // }
     // 작품 정보를 반환해줄 구조체
     struct ArtTokenData{
-        uint id;
-        string title;
-        string artist;
-        string desc;
+        uint artTokenId;
+        string artUri;
+        uint artPrice;
     }
 
     // 예술가 여부를 매핑함. O(1)
     mapping(address => bool) private _artistAddress;
-    // 작품 정보를 매핑함
-    mapping(uint256 => Art) public arts;
+    // // 작품 정보를 매핑함
+    // mapping(uint256 => Art) public arts;
+
+    mapping(uint256 => string) public arts;
 
     /**
      * @dev Throws if called by any account other than the artist.
@@ -41,6 +43,8 @@ contract SsafyNFT is ERC721Enumerable, ERC721URIStorage, Ownable {
     }
 
     constructor() ERC721("SsafyNFT", "SFT") {}
+
+    SaleArtToken public saleArtToken;
 
     function addArtistAddress(address artistAddress) public onlyOwner {
         _artistAddress[artistAddress] = true;
@@ -70,10 +74,27 @@ contract SsafyNFT is ERC721Enumerable, ERC721URIStorage, Ownable {
         return super.tokenURI(tokenId);
     }
 
-    // function create(address to, string memory _tokenURI) public onlyArtist returns (uint256) {
+    function create(address to, string memory _tokenURI) public onlyArtist returns (uint256) {
+        uint256 newTokenId = totalSupply() + 1;
+        //NFT 민팅
+        super._mint(to, newTokenId);
+
+        //토큰 아아디리에 해당하는 URI 변경
+        super._setTokenURI(newTokenId, _tokenURI);
+
+        arts[newTokenId] =  super.getTokenURI(newTokenId);
+
+        //반환
+        return newTokenId;
+    }
+
+    // function create(string memory _tokenURI, string memory _title, string memory _artist, string memory _description) public onlyArtist returns (uint256) {
     //     uint256 newTokenId = totalSupply() + 1;
+
+    //     arts[newTokenId] = Art(newTokenId, _title, _artist, _description);
+
     //     //NFT 민팅
-    //     super._mint(to, newTokenId);
+    //     super._mint(msg.sender, newTokenId);
 
     //     //토큰 아아디리에 해당하는 URI 변경
     //     super._setTokenURI(newTokenId, _tokenURI);
@@ -82,45 +103,43 @@ contract SsafyNFT is ERC721Enumerable, ERC721URIStorage, Ownable {
     //     return newTokenId;
     // }
 
-    function create(string memory _tokenURI, string memory _title, string memory _artist, string memory _description) public onlyArtist returns (uint256) {
-        uint256 newTokenId = totalSupply() + 1;
-
-        arts[newTokenId] = Art(newTokenId, _title, _artist, _description);
-
-        //NFT 민팅
-        super._mint(msg.sender, newTokenId);
-
-        //토큰 아아디리에 해당하는 URI 변경
-        super._setTokenURI(newTokenId, _tokenURI);
-
-        //반환
-        return newTokenId;
-    }
-
     function supportsInterface(bytes4 interfaceId) public view override(ERC721, ERC721Enumerable) returns (bool) {
         return super.supportsInterface(interfaceId);
     }
 
-    function _baseURI() internal pure override returns (string memory) {
-        return "https://localhost:5999/token/";
-    }
+    // function _baseURI() internal pure override returns (string memory) {
+    //     return "https://localhost:5999/token/";
+    // }
 
     // 지갑 주소에 해당하는 작품 배열 반환
     function getArtTokens(address _artTokenOwner) view public returns (ArtTokenData[] memory) {
+        
+
         uint256 balanceLength = balanceOf(_artTokenOwner);
 
         require(balanceLength != 0, "Owner did not have token.");
 
+        // ArtTokenData[] memory artTokenData = new ArtTokenData[](balanceLength);
+
         ArtTokenData[] memory artTokenData = new ArtTokenData[](balanceLength);
 
+      
         for(uint256 i = 0; i < balanceLength; i++){
             uint256 artTokenId = tokenOfOwnerByIndex(_artTokenOwner, i);
-            Art memory art = arts[artTokenId];
- 
-            artTokenData[i] = ArtTokenData(artTokenId, art.title, art.artist, art.description);
+            
+            string memory artTokenURI = super.getTokenURI(artTokenId);
+            uint256 artPrice = saleArtToken.getArtTokenPrice(artTokenId);
+
+            // artTokenData[i] = artTokenURI;
+            artTokenData[i] = ArtTokenData(artTokenId, artTokenURI, artPrice);
         }
 
         return artTokenData;
 
     }
+
+    function setSaleArtToken(address _saleArtTokens) public {
+        saleArtToken = SaleArtToken(_saleArtTokens);
+    }
+
 }

49a17c9717b7a80cf8990e2e4a7cfb9564d992b8
김재욱 <postkim03@naver.com>
2022-03-24 14:47:41 +0900

    Add Charity entity

---
+++ third_donation_server/src/main/java/com/thirdlife/thirddonation/api/service/charity/CharityServiceImpl.java
@@ -0,0 +1,62 @@
+package com.thirdlife.thirddonation.api.service.charity;
+
+import com.thirdlife.thirddonation.api.dto.request.user.UserRequest;
+import com.thirdlife.thirddonation.api.exception.CustomException;
+import com.thirdlife.thirddonation.api.exception.ErrorCode;
+import com.thirdlife.thirddonation.api.service.charity.CharityService;
+import com.thirdlife.thirddonation.db.entity.charity.Charity;
+import com.thirdlife.thirddonation.db.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * 유저 서비스의 구현체입니다.
+ */
+@Service
+@Transactional
+@AllArgsConstructor
+public class CharityServiceImpl implements CharityService {
+
+    private final UserRepository userRepository;
+
+    /**
+     * 자선단체를 생성하고 DB에 저장하는 메서드입니다.
+     *
+     * @param userRequest UserLoginRequestDto
+     * @return Charity
+     */
+    @Override
+    public Charity createCharity(UserRequest userRequest) throws CustomException {
+        if (checkDuplicateWalletAddress(userRequest.getWalletAddress())) {
+            throw new CustomException(ErrorCode.USER_ID_DUPLICATE);
+        }
+
+        User userEntity = userRequest.toEntity();
+
+        System.out.println(userEntity);
+        userEntity.setPrivateHash(passwordEncoder.encode(userRequest.getPrivateHash()));
+        try {
+            userRepository.save(userEntity);
+        } catch (Exception ex) {
+            CustomException ex2 = new CustomException(ErrorCode.DATABASE_SERVER_ERROR);
+            throw ex2;
+        }
+
+        return userEntity;
+    }
+
+    /**
+     * 유저를 지갑 주소로 반환 받는 메서드입니다.
+     *
+     * @param Name String
+     * @return walletAddress
+     */
+    @Override
+    @Transactional(readOnly = true)
+    public Charity getWalletAddressByCharity(String Name) {
+        return userRepository.findByWalletAddress(walletAddress);
+    }
+
+}

e710af9aa1f42f7ecb6fbeff29632b4370abb9f0
redniche <redniche@naver.com>
2022-03-30 02:32:06 +0900

    fix: Fix bug in project
    Mint.js에 withCredentials 구문 제거
    중복된 라우터 제거
    다음부터 패키지 추가했으면 얘기해주길 바람!

--- third_donation_front/src/pages/board/BoardList.js
+++ third_donation_front/src/pages/board/BoardList.js
@@ -1,10 +1,8 @@
 import { useState, useEffect } from 'react';
-// import { Link } from 'react-router-dom';
 import Table from '../../components/board/Table';
 import Col from '../../components/board/Col';
 import Row from '../../components/board/Row';
 import { postList } from './Data';
-// import { Link } from '@reach/router';
 
 function NoticeWrite() {
   window.location.href = '/noticeWrite';

eac2f3bde714b8f23d16fa7cefb3200c3284cd0c
박대언 <mem29238@gmail.com>
2022-04-01 11:43:11 +0900

    Update axios
    - nginx 도입에 따른 api 호출 주소 수정

--- third_donation_front/src/core/axios.js
+++ third_donation_front/src/core/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const API_URL = 'http://j6e207.p.ssafy.io:32090/api';
+export const API_URL = 'https://j6e207.p.ssafy.io/api';
 export const API_TIME_SOURCE = 'https://worldtimeapi.org/api/timezone/Asia/Seoul';
 export const Axios = axios.create({
   baseURL: API_URL,

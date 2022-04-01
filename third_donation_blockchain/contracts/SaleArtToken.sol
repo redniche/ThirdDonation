65aca174dd749b10feb1a8cc48256fbf015b780b
김동주 <rlaehdwn23@naver.com>
2022-04-02 03:50:53 +0900

    Update purchase nft
    - NFT 구매 시 판매자, 예술가, 기부단체, 컨트랙트 소유자에게 토큰 금액 분배
    - 판매 금액 100이상으로 조건 처리
    - 자선 단체 지갑 주소 등록, 삭제, 불러오기
    - 토큰 민팅한 예술가 주소 담기, 불러오기

--- third_donation_blockchain/contracts/SaleArtToken.sol
+++ third_donation_blockchain/contracts/SaleArtToken.sol
@@ -2,13 +2,16 @@
 pragma solidity ^0.8.0;
 
 import "./SsafyNFT.sol";
+import "./SsafyToken.sol";
 
 contract SaleArtToken {
     SsafyNFT public mintArtTokenAddress;
+    SsafyToken public ssafyTokenAddress;
 
     // MintArtToken 스마트 컨트랙트 주소값으로 생성
-    constructor(address _mintArtTokenAddress){
+    constructor(address _mintArtTokenAddress, address _ssafyTokenAddress){
         mintArtTokenAddress = SsafyNFT(_mintArtTokenAddress);
+        ssafyTokenAddress = SsafyToken(_ssafyTokenAddress);
     }
 
     mapping(uint256 => uint256) public artTokenPrices;
@@ -22,7 +25,7 @@ contract SaleArtToken {
         address artTokenOwner = mintArtTokenAddress.ownerOf(_artTokenId);
         
         require(artTokenOwner == msg.sender, "Caller is not art token owner.");
-        require(_price > 0, "Price is zero or lower.");
+        require(_price >= 100, "Price must be at least 100.");
         // 해당 작품이 판매중이 아닐 때
         require(artTokenPrices[_artTokenId] == 0, "This art token is already on sale");
         // artTokenOwner가 판매 스마트 컨트랙트(address(this))에 권한을 넘겼는지 확인
@@ -50,18 +53,30 @@ contract SaleArtToken {
 
         }
 
-    }
+    } 
 
     // 작품 구매
-    function purchaseArtToken(uint256 _artTokenId) public payable{
+    function purchaseArtToken(uint256 _artTokenId, address sender, address charity) public {
         uint256 price = artTokenPrices[_artTokenId];
         address artTokenOwner = mintArtTokenAddress.ownerOf(_artTokenId);
 
         require(price > 0, "Art Token is not sale.");
-        require(price <= msg.value, "Caller sent lower than price.");
+        // require(price <= msg.value, "Caller sent lower than price.");
         require(artTokenOwner != msg.sender, "Caller is art token owner.");
 
-        payable(artTokenOwner).transfer(msg.value);
+        uint256 balance = ssafyTokenAddress.balanceOf(sender);
+        require(price<balance, "Caller sent lower than price");
+
+        bool ischarity = mintArtTokenAddress.getCharity(charity);
+        require(ischarity, "Charity is not registed");
+
+        address tokenArtist = mintArtTokenAddress.getTokenArtistAddress(_artTokenId);
+        address contractOwner = getContractOwner();
+        ssafyTokenAddress.transferFrom(sender, artTokenOwner, price * 90 / 100); // 토큰 소유자에게 금액 전송
+        ssafyTokenAddress.transferFrom(sender, tokenArtist, price * 6 / 100); // 토큰 민팅한 예술가에게 금액 전송
+        ssafyTokenAddress.transferFrom(sender, charity, price * 2 / 100); // 기부단체에게 금액 전송
+        ssafyTokenAddress.transferFrom(sender, contractOwner, price * 2 / 100); // 컨트랙트 소유자에게 금액 전송
+
         mintArtTokenAddress.safeTransferFrom(artTokenOwner, msg.sender, _artTokenId);
 
         artTokenPrices[_artTokenId] = 0;
@@ -71,17 +86,12 @@ contract SaleArtToken {
                 onSaleArtTokkenArray[i] = onSaleArtTokkenArray[onSaleArtTokkenArray.length-1];
                 onSaleArtTokkenArray.pop();
             }
-
         }
     }
 
-    // 판매 중인 작품 배열의 길이 출력
-    function getOnSaleArtTokenArrayLength() view public returns (uint256) {
-        return onSaleArtTokkenArray.length;
-    }
-
-    function getArtTokenPrice(uint256 _artTokenId) view public returns (uint256){
-        return artTokenPrices[_artTokenId];
+    
+    function getContractOwner() view public returns (address){
+        return mintArtTokenAddress.owner();
     }
 
 }

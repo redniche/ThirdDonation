33d0a0fc0ecdfa8d3b052104ea95009455065b90
박대언 <mem29238@gmail.com>
2022-04-03 02:51:56 +0900

    Fix notification repository
    - 알림 리포지터리 누락 어노테이션 추가

--- third_donation_server/src/main/java/com/thirdlife/thirddonation/db/notification/repository/NotificationRepository.java
+++ third_donation_server/src/main/java/com/thirdlife/thirddonation/db/notification/repository/NotificationRepository.java
@@ -4,6 +4,7 @@ import com.thirdlife.thirddonation.db.notification.entity.Notification;
 import com.thirdlife.thirddonation.db.user.entity.User;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -13,6 +14,7 @@ import org.springframework.data.repository.query.Param;
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
     List<Notification> findByUserAndEnabled(User user, Boolean enabled);
 
+    @Modifying
     @Query(value = "update Notification n set n.enabled = :enabled where n.user = :user")
     void setEnabledByUser(@Param("user") User user, @Param("enabled") Boolean enabled);
 }

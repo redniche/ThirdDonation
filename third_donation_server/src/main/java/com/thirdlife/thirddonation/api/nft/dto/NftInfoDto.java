22d3dec7a286d8eb50c8bd8a42b366364aefbf75
redniche <redniche@naver.com>
2022-04-04 23:37:16 +0900

    fix: Fix bugin in NftInfoDto
    사소하지만 급한 버그 해결

--- third_donation_server/src/main/java/com/thirdlife/thirddonation/api/nft/dto/NftInfoDto.java
+++ third_donation_server/src/main/java/com/thirdlife/thirddonation/api/nft/dto/NftInfoDto.java
@@ -43,7 +43,7 @@ public class NftInfoDto {
                 .isMintSold(nft.getIsMintSold())
                 .wishCount(nft.getWishCount())
                 .name(nft.getName())
-                .name(nft.getFileType())
+                .fileType(nft.getFileType())
                 .build();
     }
 }

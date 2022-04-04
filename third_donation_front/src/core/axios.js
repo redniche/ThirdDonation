f8358e7a9a8fb41e8e164729251ecef1f66970f9
redniche <redniche@naver.com>
2022-04-04 23:43:53 +0900

    Update apis name, some layout
    apis 이름 변경, 일부 레이아웃 변경

--- third_donation_front/src/core/axios.js
+++ third_donation_front/src/core/axios.js
@@ -13,7 +13,7 @@ const user_base = '/users';
 const nft_sale_base = '/nfts/exchange';
 const file_base = '/upload/file/';
 
-const apis = {
+const axios_apis = {
   nfts: {
     items: `${nft_base}/items`,
     list: `${nft_base}/nft`,
@@ -36,4 +36,4 @@ export const setAxiosHeader = (data) => {
 //처음 시작시 단 한 번 실행
 setAxiosHeader(auth.getToken());
 
-export default apis;
+export default axios_apis;
--- third_donation_front/src/core/ipfs.js
+++ third_donation_front/src/core/ipfs.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { create } from 'ipfs-http-client';
 
-const apis = {
+const ipfs_apis = {
   https_public: 'https://ipfs.io/ipfs',
   https_local: 'https://j6e207.p.ssafy.io/ipfs',
   ipfs: 'ipfs://',
@@ -14,14 +14,14 @@ export const Ipfs = create({
 });
 
 export const IpfsAxios = axios.create({
-  baseURL: apis.https_local,
+  baseURL: ipfs_apis.https_local,
 });
 
 export const convertIpfsToHttps = (ipfsLink) => {
   if (typeof ipfsLink == 'string') {
-    return ipfsLink.replace(apis.ipfs, apis.https_local + '/');
+    return ipfsLink.replace(ipfs_apis.ipfs, ipfs_apis.https_local + '/');
   }
   //문자열이 아니면 null 반환
 };
 
-export default apis;
+export default ipfs_apis;

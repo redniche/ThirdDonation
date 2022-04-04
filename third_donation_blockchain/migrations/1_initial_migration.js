089e9ab87ae230ae45b8621315b03e42871751d1
김동주 <rlaehdwn23@naver.com>
2022-04-04 09:28:00 +0900

    Update initial migration
    - 판매 컨트랙트 배포 코드 추가

--- third_donation_blockchain/migrations/1_initial_migration.js
+++ third_donation_blockchain/migrations/1_initial_migration.js
@@ -1,7 +1,9 @@
 const Migrations = artifacts.require('Migrations');
 const SsafyNFT = artifacts.require('SsafyNFT');
+const SaleArtToken = artifacts.require('SaleArtToken');
 
 module.exports = function (deployer) {
   deployer.deploy(Migrations);
   deployer.deploy(SsafyNFT);
+  deployer.deploy(SaleArtToken);
 };
